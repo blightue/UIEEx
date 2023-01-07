454be4f30ae81146cb3a9e00610a2136e7f33507
suisuishou <blightue@gmail.com>
2023-01-07 15:00:33 +0800

    Split UIELayout Engine and UIELayout Editor input path in codegen window Add Script hint to every script

--- Scripts/Editor/CodeGenerator/UIETypeNameSO.cs
+++ Scripts/Editor/CodeGenerator/UIETypeNameSO.cs
@@ -5,8 +5,8 @@ using UnityEngine.Serialization;
 [CreateAssetMenu(menuName = "UIEEx/Code Gen/Elements")]
     public class UIETypeNameSO : ScriptableObject
     {
-        [FormerlySerializedAs("EngineElementNames")] [SerializeField] public string[] EngineElementTypeNames;
-        [FormerlySerializedAs("EditorElementNames")] [SerializeField] public string[] EditorElementTypeNames;
+        [SerializeField] public string[] EngineElementTypeNames;
+        [SerializeField] public string[] EditorElementTypeNames;
         
         // public string[] EngineElementNames => engineElementNames.Select(a => "UnityEngine.UIElements." + a).ToArray();
         // public string[] EditorElementNames => editorElementNames.Select(a => "UnityEditor.UIElements." + a).ToArray();
